6ed2fa1f98a5a0fa9ede24a1b412c0045c9d4d26
Radhouane <1804433@cmontmorency.qc.ca>
2025-04-23 17:56:04 -0400

    commit_modif_script osc

--- Os_Scape/Assets/OSC_Script.cs
+++ Os_Scape/Assets/OSC_Script.cs
@@ -4,8 +4,36 @@ using extOSC;
 using UnityEngine;
 
 
-    public class OSC_Script : MonoBehaviour
+    public class OSCScript : MonoBehaviour
     {
         public OSCReceiver oscReceiver;
         public OSCTransmitter oscTransmitter;
+    private int maValeur;
+    private float myChrono;
+
+
+    private void LateUpdate()
+    {
+        // Si 100 millisecondes se sont écoulées depuis le dernier envoi :
+        if (Time.realtimeSinceStartup - myChrono >= 0.1f)
+        {
+            myChrono = Time.realtimeSinceStartup;
+
+            // Créer le message
+            var myOscMessage = new OSCMessage("/adresse");
+
+            // Ajouter une valeur au message, maValeur doit être remplacé par le int de votre jeu que vous souhaitez envoyer.
+            myOscMessage.AddValue(OSCValue.Int((int)maValeur)); // Le (int) entre parenthèses convertit le type.
+
+            // Envoyer le message
+            oscTransmitter.Send(myOscMessage);
+
+
+        }
     }
+    private void Start()
+    {
+        oscReceiver.Bind("*", TraiterMessageOSC);
+    }
+}
+

f8ec89c06ae2a1c4c67198f3d92ca1635a9b4873
Radhouane <1804433@cmontmorency.qc.ca>
2025-04-23 17:17:08 -0400

    commit_ajout_OSC_et_Script

---
+++ Os_Scape/Assets/OSC_Script.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using extOSC;
+using UnityEngine;
+
+
+    public class OSC_Script : MonoBehaviour
+    {
+        public OSCReceiver oscReceiver;
+        public OSCTransmitter oscTransmitter;
+    }

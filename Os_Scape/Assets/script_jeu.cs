ae419400938312d0afb6e1cfa4336fcf26d4c172
Radhouane <1804433@cmontmorency.qc.ca>
2025-05-10 18:02:17 -0400

    modif script jeu
    ajout de code en commentaire, mais à modifier

--- Os_Scape/Assets/script_jeu.cs
+++ Os_Scape/Assets/script_jeu.cs
@@ -15,4 +15,43 @@ public class Script_jeu : MonoBehaviour
         Application.Quit();
     }
 
+
+    /*
+void OnTriggerEnter(Collider other)
+{
+    if (other.CompareTag("zone1"))
+    {
+        ampoule.SetActive(true);
+    }
+    else if (other.CompareTag("zone3"))
+    {
+        videoPlayer.Play();
+    }
+    else if (other.CompareTag("zone2"))
+    {
+        animLumiere.Play("fadeIn");
+    }
+    else if (other.CompareTag("etoile"))
+    {
+        other.gameObject.SetActive(false);
+        count++;
+        pointage.OnChangerPointage(count);
+    }
+}
+void OnTriggerExit(Collider other)
+{
+    if (other.CompareTag("zone1"))
+    {
+        ampoule.SetActive(false);
+    }
+    else if (other.CompareTag("zone3"))
+    {
+        videoPlayer.Stop();
+    }
+    else if (other.CompareTag("zone2"))
+    {
+        animLumiere.Play("fadeOut");
+    }
+}
+*/
 }

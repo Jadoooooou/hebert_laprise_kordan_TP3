c16240468e71f4092960bc278e8a35bdb6366642
lapriseml <143807655+lapriseml@users.noreply.github.com>
2025-05-11 16:25:42 -0400

    niveau1 - vies script

--- Os_Scape/Assets/script_jeu.cs
+++ Os_Scape/Assets/script_jeu.cs
@@ -8,6 +8,38 @@ public class Script_jeu : MonoBehaviour
     public GameObject porteGauche;
     public GameObject porteDroite;
 
+    public AudioSource myAudioSource;
+
+    public GameObject[] vies;
+    public int decompteVies;
+
+    public void jouer()
+    {
+        SceneManager.LoadScene("Niveau1");
+    }
+    public void quitterPartie()
+    {
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        decompteVies = 3;
+    }
+
+    void PerteVies(Collider other)
+    {
+        if (other.CompareTag("???"))
+        {
+            decompteVies -= 1;
+            vies[decompteVies].SetActive(false);
+        }
+        if (decompteVies == 0)
+        {
+            //recommencer niveau1
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("h1porteGauche"))
@@ -21,4 +53,15 @@ public class Script_jeu : MonoBehaviour
             porteGauche.SetActive(true);
         }
     }
+
+    public void VolumeOn() 
+    {
+        myAudioSource.Play();
+    }
+
+    public void VolumeOff()
+    {
+        myAudioSource.Stop();
+    }
 }
+

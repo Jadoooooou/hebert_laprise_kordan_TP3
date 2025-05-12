835ad898023bc3aef5c83f47ed0bdae53db06541
Rad8433 <1804433@cmontmorency.qc.ca>
2025-05-11 20:16:50 -0400

    modif script collision + animation plateforme

--- Os_Scape/Assets/script_collision.cs
+++ Os_Scape/Assets/script_collision.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StoneCollision : MonoBehaviour
 {
     public GameObject crate;
-    public Animation animPlateform;  // Animation component on the platform
+    public Animator animPlatform; 
 
     private bool hasPlayed = false;
 
@@ -17,15 +15,7 @@ public class StoneCollision : MonoBehaviour
             {
                 Debug.Log("Stone is on top of the crate!");
 
-                if (animPlateform != null)
-                {
-                    animPlateform.Play("plateform");  // ? match your clip name exactly
-                    hasPlayed = true;
-                }
-                else
-                {
-                    Debug.LogWarning("Animation component not assigned to animPlateform!");
-                }
+                animPlatform.SetTrigger("PlayAnim"); 
             }
         }
     }

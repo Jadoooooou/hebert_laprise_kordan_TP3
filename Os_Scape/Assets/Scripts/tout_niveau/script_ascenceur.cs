bb7e1c49f05fe7fffaa0a348c5b6459df8e56aa7
Rad8433 <1804433@cmontmorency.qc.ca>
2025-05-14 13:56:20 -0400

    Script_ascenceur corrigé + quelques améliorations

--- Os_Scape/Assets/Scripts/tout_niveau/script_ascenceur.cs
+++ Os_Scape/Assets/Scripts/tout_niveau/script_ascenceur.cs
@@ -2,23 +2,22 @@ using UnityEngine;
 
 public class PlatformParenting1 : MonoBehaviour
 {
-    public GameObject joueur;  // Reference to the player
     public GameObject empty;   // Neutral object to unparent to
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == joueur)
+        if (other.CompareTag("Player"))
         {
-            joueur.transform.parent = transform.parent; // Set player as child of the platform
+            other.transform.parent = transform.parent; // Set player as child of the platform
             Debug.Log("Player entered platform trigger. Player is now child of: " + transform.parent.name);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == joueur)
+        if (other.CompareTag("Player"))
         {
-            joueur.transform.parent = empty.transform; // Unparent to empty
+            other.transform.parent = empty.transform; // Unparent to empty
             Debug.Log("Player exited platform trigger. Player is now child of: " + empty.name);
         }
     }

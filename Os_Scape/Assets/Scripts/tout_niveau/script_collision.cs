0f97ad3c903cf7de20f65fce1ab30d42625f5b8c
Rad8433 <1804433@cmontmorency.qc.ca>
2025-05-14 00:32:34 -0400

    amélioration du script collision

--- Os_Scape/Assets/Scripts/tout_niveau/script_collision.cs
+++ Os_Scape/Assets/Scripts/tout_niveau/script_collision.cs
@@ -3,20 +3,44 @@ using UnityEngine;
 public class StoneCollision : MonoBehaviour
 {
     public GameObject crate;
-    public Animator animPlatform;
+    public Animator animPlateform;
 
+    private bool isTouchingCrate = false;
     private bool isStoneOnCrate = false;
+
+    public float heightThreshold = 0.5f;  // How much higher the stone must be above the crate
 
     void Update()
     {
-        if (isStoneOnCrate)
+        if (isTouchingCrate)
         {
-            if (transform.position.y <= crate.transform.position.y + 0.5f)
+            // Check if the stone is truly on top
+            if (transform.position.y > crate.transform.position.y + heightThreshold)
             {
-                Debug.Log("Stone is no longer on top of the crate.");
-                animPlatform.SetTrigger("CloseAnim");
-                isStoneOnCrate = false; 
+                if (!isStoneOnCrate)
+                {
+                    Debug.Log("Stone is now on top of the crate.");
+                    animPlateform.SetTrigger("PlayAnim");
+                    isStoneOnCrate = true;
+                }
             }
+            else
+            {
+                // Still touching, but no longer on top
+                if (isStoneOnCrate)
+                {
+                    Debug.Log("Stone fell off the crate.");
+                    animPlateform.SetTrigger("Idle");
+                    isStoneOnCrate = false;
+                }
+            }
+        }
+        else if (isStoneOnCrate)
+        {
+            // No longer touching at all
+            Debug.Log("Stone is no longer touching the crate.");
+            animPlateform.SetTrigger("Idle");
+            isStoneOnCrate = false;
         }
     }
 
@@ -24,13 +48,15 @@ public class StoneCollision : MonoBehaviour
     {
         if (collision.gameObject == crate)
         {
-            if (transform.position.y > crate.transform.position.y + 0.5f && !isStoneOnCrate)
-            {
-                Debug.Log("Stone is on top of the crate!");
+            isTouchingCrate = true;
+        }
+    }
 
-                animPlatform.SetTrigger("PlayAnim");
-                isStoneOnCrate = true; 
-            }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == crate)
+        {
+            isTouchingCrate = false;
         }
     }
 }

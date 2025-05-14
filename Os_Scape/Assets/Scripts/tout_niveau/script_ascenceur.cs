14dc284e7a7009416bc64c4be3715f365267f5ae
Rad8433 <1804433@cmontmorency.qc.ca>
2025-05-14 04:03:41 -0400

    Osc cam et debug ascenceur

--- Os_Scape/Assets/Scripts/tout_niveau/script_ascenceur.cs
+++ Os_Scape/Assets/Scripts/tout_niveau/script_ascenceur.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class PlatformParenting1 : MonoBehaviour
 {
-    public GameObject joueur;          // Reference to the player
-    public GameObject empty;           // Neutral object to unparent to
+    public GameObject joueur;  // Reference to the player
+    public GameObject empty;   // Neutral object to unparent to
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == joueur)
         {
             joueur.transform.parent = transform.parent; // Set player as child of the platform
+            Debug.Log("Player entered platform trigger. Player is now child of: " + transform.parent.name);
         }
     }
 
@@ -18,6 +19,7 @@ public class PlatformParenting1 : MonoBehaviour
         if (other.gameObject == joueur)
         {
             joueur.transform.parent = empty.transform; // Unparent to empty
+            Debug.Log("Player exited platform trigger. Player is now child of: " + empty.name);
         }
     }
 }

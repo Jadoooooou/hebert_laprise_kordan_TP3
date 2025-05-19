7f09340eaa58ea75e937263497253535d3653384
lapriseml <143807655+lapriseml@users.noreply.github.com>
2025-05-19 16:53:25 -0400

    Vies - tous niveaux

--- Os_Scape/Assets/Scripts/tout_niveau/script_jeu.cs
+++ Os_Scape/Assets/Scripts/tout_niveau/script_jeu.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Script_jeu : MonoBehaviour
+public class Joueur : MonoBehaviour
 {
-
     public GameObject[] vies;
     public int decompteVies;
 
     public GameObject defaite;
 
+    private bool isInvincible = false;
+
     public void jouer()
     {
         SceneManager.LoadScene("Niveau1");
     }
+
     public void quitterPartie()
     {
         Application.Quit();
@@ -30,19 +32,29 @@ public class Script_jeu : MonoBehaviour
         decompteVies = 3;
     }
 
-
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("lave"))
+        if (other.CompareTag("lave") && !isInvincible)
         {
             decompteVies -= 1;
             vies[decompteVies].SetActive(false);
-        }
-        if (decompteVies == 0)
-        {
-            defaite.SetActive(true);
+
+            if (decompteVies == 0)
+            {
+                defaite.SetActive(true);
+            }
+            else
+            {
+                StartCoroutine(ActiverInvincibilite());
+            }
         }
     }
-}
 
+    IEnumerator ActiverInvincibilite()
+    {
+        isInvincible = true;
+        yield return new WaitForSeconds(2f);
+        isInvincible = false;
+    }
+}
 
